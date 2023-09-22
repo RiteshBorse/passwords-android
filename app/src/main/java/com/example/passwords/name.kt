262bb24af725026883e97f4c4747958f30a44288
RiteshBorse <riteshborse008@gmail.com>
2023-09-22 06:27:11 +0530

    UI to set name

---
+++ app/src/main/java/com/example/passwords/name.kt
@@ -0,0 +1,11 @@
+package com.example.passwords
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class name : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_name)
+    }
+}
