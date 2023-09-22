fda33150299d8d96e30518b59bbc32974c40b022
RiteshBorse <riteshborse008@gmail.com>
2023-09-22 19:22:27 +0530

    UI to add password

---
+++ app/src/main/java/com/example/passwords/addpassword.kt
@@ -0,0 +1,11 @@
+package com.example.passwords
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class addpassword : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_addpassword)
+    }
+}
