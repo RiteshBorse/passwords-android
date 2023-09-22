a1b1ca164d283260f6ef5768f4de1841d8bd7412
RiteshBorse <riteshborse008@gmail.com>
2023-09-22 06:47:33 +0530

    Ui to Confirm Password

---
+++ app/src/main/java/com/example/passwords/confirm_password.kt
@@ -0,0 +1,11 @@
+package com.example.passwords
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class confirm_password : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_confirm_password)
+    }
+}
