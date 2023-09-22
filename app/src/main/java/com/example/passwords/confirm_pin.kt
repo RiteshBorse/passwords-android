a283ae43fa0dba5f340223d753932fe1932225d7
RiteshBorse <riteshborse008@gmail.com>
2023-09-22 06:41:15 +0530

    UI to Confirm Pin

---
+++ app/src/main/java/com/example/passwords/confirm_pin.kt
@@ -0,0 +1,11 @@
+package com.example.passwords
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class confirm_pin : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_confirm_pin)
+    }
+}
