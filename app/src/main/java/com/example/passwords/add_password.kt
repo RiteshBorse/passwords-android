4cebd2ef5e75309008f771730ac565e859b676e4
RiteshBorse <riteshborse008@gmail.com>
2023-10-08 21:21:24 +0530

    Merge conflict solved
    addpassword changed to add_password

--- app/src/main/java/com/example/passwords/add_password.kt
+++ app/src/main/java/com/example/passwords/add_password.kt
@@ -3,7 +3,7 @@ package com.example.passwords
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class addpassword : AppCompatActivity() {
+class add_password : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_addpassword)
