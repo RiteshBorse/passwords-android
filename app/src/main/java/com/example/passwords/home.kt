67d7d78daf43559e3222abae7932daeb4f4c59ec
kaveriAnilk <kaverianilk2005@gmail.com>
2023-10-21 14:21:18 +0530

    Intent for home.kt

--- app/src/main/java/com/example/passwords/home.kt
+++ app/src/main/java/com/example/passwords/home.kt
@@ -1,11 +1,27 @@
 package com.example.passwords
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 
 class home : AppCompatActivity() {
+    lateinit var addPassword : ImageButton
+    lateinit var viewPassword : ImageButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        addPassword = findViewById(R.id.button_addpassword)
+        viewPassword = findViewById(R.id.button_viewpassword)
+
+        addPassword.setOnClickListener{
+            val intent = Intent(this@home,add_password::class.java)
+            startActivity(intent)
+        }
+        viewPassword.setOnClickListener{
+            val intent = Intent(this@home,save_password1::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/passwords/set_pin.kt
+++ app/src/main/java/com/example/passwords/set_pin.kt
@@ -2,13 +2,12 @@ package com.example.passwords
 
 import android.content.Intent
 import android.graphics.Color
-import android.media.Image
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Environment
 import android.widget.EditText
 import android.widget.ImageButton
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import java.io.File
 
 class set_pin : AppCompatActivity() {
