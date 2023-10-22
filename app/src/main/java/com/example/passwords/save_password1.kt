ae5d45b59a146ead91907809b1fb9d1ba1fddb1a
RiteshBorse <riteshborse008@gmail.com>
2023-10-22 15:51:37 +0530

    Incomplete

--- app/src/main/java/com/example/passwords/save_password1.kt
+++ app/src/main/java/com/example/passwords/save_password1.kt
@@ -2,11 +2,28 @@ package com.example.passwords
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
+import com.example.passwords.DB_Passwords.MyDatabaseHelper
+import com.example.passwords.DB_Passwords.Password
+import com.example.passwords.View_Passwords.MyAdapter
+import java.util.ArrayList
 
 class save_password1 : AppCompatActivity() {
+    lateinit var recyclerView: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_savepassword1)
+        recyclerView = findViewById(R.id.recyclerView)
 
+        // Initialize the database helper
+        val dbHelper = MyDatabaseHelper(this)
+
+        // Retrieve password data from the database
+        val dataList: List<Password> = dbHelper.getAllPassword()
+
+        // Create and set the adapter
+        val adapter = MyAdapter(dataList)
+        recyclerView.adapter = adapter
     }
 }
