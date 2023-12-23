f7879550a9fbe5b42b66dd31b02985ec32c0edfb
RiteshBorse <riteshborse008@gmail.com>
2023-12-23 21:28:51 +0530

    set_pin line 50 encode function added

--- app/src/main/java/com/example/passwords/home.kt
+++ app/src/main/java/com/example/passwords/home.kt
@@ -1,6 +1,7 @@
 package com.example.passwords
 
 import android.content.Intent
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageButton
@@ -8,12 +9,14 @@ import android.widget.ImageButton
 class home : AppCompatActivity() {
     lateinit var addPassword : ImageButton
     lateinit var viewPassword : ImageButton
+    lateinit var about : ImageButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         addPassword = findViewById(R.id.button_addpassword)
         viewPassword = findViewById(R.id.button_viewpassword)
+        about = findViewById(R.id.about)
 
         addPassword.setOnClickListener{
             val intent = Intent(this@home,add_password::class.java)
@@ -21,6 +24,10 @@ class home : AppCompatActivity() {
         }
         viewPassword.setOnClickListener{
             val intent = Intent(this@home,save_password1::class.java)
+            startActivity(intent)
+        }
+        about.setOnClickListener{
+            val intent = Intent(this@home,about::class.java)
             startActivity(intent)
         }
     }
--- app/src/main/java/com/example/passwords/set_pin.kt
+++ app/src/main/java/com/example/passwords/set_pin.kt
@@ -9,11 +9,9 @@ import android.widget.ImageButton
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import java.io.File
-import java.security.SecureRandom
+import androidx.activity.BackEventCompat
+import java.nio.charset.Charset
 import java.util.Base64
-import javax.crypto.Cipher
-import javax.crypto.SecretKeyFactory
-import javax.crypto.spec.PBEKeySpec
 
 class set_pin : AppCompatActivity() {
 
@@ -25,9 +23,6 @@ class set_pin : AppCompatActivity() {
 
         pin = findViewById(R.id.edt_pin)
         proceed = findViewById(R.id.button_proceed)
-
-        val modulus = 10007
-        val key = 7
 
         val fileName = "passwords_pin.txt"
         val storageDir = getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS)
@@ -52,7 +47,7 @@ class set_pin : AppCompatActivity() {
         }
 
         proceed.setOnClickListener {
-           file.writeText(pin.text.toString().trim())
+           file.writeText(encode(pin.text.toString().trim()))
             if(file.readText().isNotEmpty())
             {
                 val intent = Intent(this@set_pin,confirm_pin::class.java)
@@ -67,5 +62,13 @@ class set_pin : AppCompatActivity() {
 
         }
 
+    }
+    fun encode(text : String) : String
+    {
+        return Base64.getEncoder().encodeToString(text.toByteArray(Charsets.UTF_8))
+    }
+    fun decode(text : String) : String
+    {
+        return String(Base64.getDecoder().decode(text), Charsets.UTF_8)
     }
 }
